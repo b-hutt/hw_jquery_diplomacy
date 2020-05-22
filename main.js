5a7d944ebdb186983976bb806ba08af9b2404789
Brittney Huttner <brittneyhuttner@gmail.com>
2020-05-22 16:43:58 +0000

    Fixed color issue and number feedback

--- main.js
+++ main.js
@@ -6,18 +6,19 @@ function changeColor() {
   const colorChangeEls = $('.color-change');
 
   for (const el of colorChangeEls) {
-    $(el).toggleClass('hide');
+    $(el).toggleClass('red');
   }
 }
 
 function validateNumber(evt) {
   evt.preventDefault();
 
-  const numberInput = $('input[name="number"]');
+  const numberInput = $('input[name = "number"]');
   const userNum = parseInt(numberInput.val(), 10);  // typecast to num
 
   const formFeedback = $('#formFeedback');
-  if (!userNum) {
+
+    if (!userNum || userNum > 10) {
     formFeedback.text('Please enter a smaller number');
   } else {
     formFeedback.text('You are good to go!');
